046c35baa3ffadf132ed8fd2ab7c07ad5c9f1349
Alexandre Santos <santosam@santosam-desktop.(none)>
2014-01-03 16:34:33 +0000

    first commit. finished visuals, going to the model

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kraken_Example_Shopping_Cart
+
+"Testing"
---
+++ controllers/setLanguage.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+//var SetlanguageModel = require('../models/setLanguage');
+
+
+module.exports = function (server) {
+
+    //var model = new SetlanguageModel();
+
+
+    server.get('/setlanguage/:lang', function (req, res) {
+        
+        res.cookie('language', req.param('lang'));
+        res.redirect('/')
+        
+    });
+
+};
